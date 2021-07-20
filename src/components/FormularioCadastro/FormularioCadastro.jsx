c9e22c904e5a29e5a25764ef7f7d48bd535ba27a
Cleriston <novaes.cleriston@gmail.com>
2021-07-20 15:20:57 -0300

    first git

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import DadosPessoais from "../DadosPessoais";
+
+function FormularioCadastro({aoEnviar, validarCPF}) {
+
+
+  return (
+    <DadosPessoais/>
+   );
+}
+export default FormularioCadastro;
